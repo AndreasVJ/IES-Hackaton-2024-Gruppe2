19418fbabf882639e903d60fa2e4b6e7ff7c360f
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-02-03 14:59:01 +0100

    chore

--- despenser/lib/Old/Old.cpp
+++ despenser/lib/Old/Old.cpp
@@ -13,17 +13,19 @@ Old::Old(String SSID, String Password,String key, String url){
 }
 
 void Old::oldInit(){
-    FirebaseData fbdo;
-    FirebaseAuth auth;
-    FirebaseConfig config;
-    int count = 0;
-    Serial.begin(115200);
+  FirebaseData fbdo;
+  FirebaseAuth auth;
+  FirebaseConfig config;
+  int count = 0;
+  Serial.begin(115200);
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(300);
   }
+  
   Serial.println();
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
--- despenser/src/main.cpp
+++ despenser/src/main.cpp
@@ -18,16 +18,7 @@
 
 RFID_ID_Scanner* id_scanner;
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-Old Oldperson("SSID", "Password", "key", "URL");
-
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
-
-FirebaseData fbdo;
-FirebaseAuth auth;
-FirebaseConfig config;
-
-bool hasWritten = false;
+Old* drugfetcher;
 
 void setup() {
   Serial.begin(9600);
@@ -36,28 +27,7 @@ void setup() {
   // put your setup code here, to run once:
   id_scanner = new RFID_ID_Scanner();
 
-  WiFi.begin("AndreasVJ iPhone", "12345679");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-
-  Serial.println("Connected to WiFi");
-
-  config.api_key = "AIzaSyBMdIr9yelGR95c8WQnh93mpVvEY9NRrss";
-  auth.user.email = "gamlis@uid.com";
-  auth.user.password = "gamlis";
-
-  Firebase.reconnectNetwork(true);
-
-  fbdo.setBSSLBufferSize(4096, 1024);
-  fbdo.setResponseSize(2048);
-
-  Firebase.begin(&config, &auth);
-
-  // Initialize an NTPClient to get time
-  timeClient.begin();
+  drugfetcher = new Old("SSID", "Password", "key", "URL");
 }
 
 void loop() {
@@ -65,30 +35,7 @@ void loop() {
   String scanned_id = id_scanner->Scan();
   Serial.print(scanned_id);
 
-  if (Firebase.ready() && hasWritten == false) {
-      hasWritten = true;
-      timeClient.update();
-
-      // Get the epoch time (seconds since January 1, 1970)
-      unsigned long epochTime = timeClient.getEpochTime();
-      Serial.println(epochTime);
-
-
-      FirebaseJson content;
-      String documentPath = "test/" + String(epochTime);
-
-      String dataPath = "fields/time/integerValue";
-      content.set(dataPath, epochTime);
-
-      if (Firebase.Firestore.createDocument(&fbdo, "ies-hackaton-2024-gruppe2", "", documentPath.c_str(), content.raw()))
-        Serial.println("Data uploaded successfully");
-      else
-        Serial.println("Failed to upload data");
-
-      delay(1000);
-    }
-
-    delay(100);
+  delay(100);
 }
 
 
